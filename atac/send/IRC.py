25f17fd0065c7f55a2deaa4dd252a0ea03c32802
strikles <strikles@gmail.com>
2022-07-30 23:34:36 +0200

    comments

--- atac/send/IRC.py
+++ atac/send/IRC.py
@@ -1,26 +1,3 @@
-# Copyright (c) Twisted Matrix Laboratories.
-# See LICENSE for details.
-
-
-"""
-An example IRC log bot - logs a channel's events to a file.
-
-If someone says the bot's name in the channel followed by a ':',
-e.g.
-
-  <foo> logbot: hello!
-
-the bot will reply:
-
-  <logbot> foo: I am a log bot
-
-Run this script with two arguments, the channel name the bot should
-connect to, and file to log to, e.g.:
-
-  $ python ircLogBot.py test test.log
-
-will log channel #test to the file 'test.log'.
-"""
 # system imports
 import magic
 import random
