96f4c52cfca4abf03118eb8d15289648d8a3c7a3
x7E3 <x7E3@icloud.com>
2022-11-08 07:33:15 +0100

    rapists

--- atac/tests/test_irc.py
+++ atac/tests/test_irc.py
@@ -1,8 +1,10 @@
+import atac
+
 #@pytest.mark.skip(reason="we fight spam :)")
 def test_send_email():
     """ """
     #
     katie = atac.SendIRC(encrypted_config, config_file, key_file)
-    status = katie.send(msg)
+    status = katie.send()
 
     assert (status == 0) is True
