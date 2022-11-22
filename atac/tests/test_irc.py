f371741ccf3fc5fd929fa42e39469073e85fd4d0
x7E3 <x7E3@icloud.com>
2022-11-22 08:36:32 +0100

    test_irc

--- atac/tests/test_irc.py
+++ atac/tests/test_irc.py
@@ -1,10 +1,10 @@
 import atac
 
 #@pytest.mark.skip(reason="we fight spam :)")
-def test_send_email():
+def test_send_irc():
     """ """
     #
     katie = atac.SendIRC(False, "auth.json", None)
-    status = katie.send()
+    status = katie.send(None)
 
     assert (status == 0) is True
