0c1b2a64dacd022fefd8d2a0d0a6b5525d835247
x7E3 <x7E3@icloud.com>
2022-11-22 08:49:58 +0100

    irc servers

--- atac/config/settings/irc_settings.py
+++ atac/config/settings/irc_settings.py
@@ -66,7 +66,7 @@ responses = [
 ]
 networks = [
     dict(
-        active=False,
+        active=True,
         nickname="pax",
         password="",
         port=6667,
@@ -74,7 +74,7 @@ networks = [
         ssl=False,
     ),
     dict(
-        active=False,
+        active=True,
         nickname="pax",
         password="",
         port=6667,
@@ -82,7 +82,7 @@ networks = [
         ssl=False,
     ),
     dict(
-        active=False,
+        active=True,
         nickname="pax",
         password="",
         port=6667,
@@ -90,7 +90,7 @@ networks = [
         ssl=False,
     ),
     dict(
-        active=False,
+        active=True,
         nickname="pax",
         password="",
         port=6667,
@@ -98,7 +98,7 @@ networks = [
         ssl=False,
     ),
     dict(
-        active=False,
+        active=True,
         nickname="pax",
         password="",
         port=6667,
@@ -106,7 +106,7 @@ networks = [
         ssl=False,
     ),
     dict(
-        active=False,
+        active=True,
         nickname="pax",
         password="",
         port=6667,
@@ -114,7 +114,7 @@ networks = [
         ssl=False,
     ),
     dict(
-        active=False,
+        active=True,
         nickname="pax",
         password="",
         port=6667,
@@ -122,7 +122,7 @@ networks = [
         ssl=False,
     ),
     dict(
-        active=False,
+        active=True,
         nickname="pax",
         password="",
         port=6667,
@@ -138,7 +138,7 @@ networks = [
         ssl=True,
     ),
     dict(
-        active=False,
+        active=True,
         nickname="pax",
         password="",
         port=6667,
