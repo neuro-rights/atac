5f6bdc92db8fcfba3c9939b66b8e0cc506564480
x7E3 <x7E3@icloud.com>
2022-11-22 08:32:57 +0100

    test_irc

--- atac/tests/test_irc.py
+++ atac/tests/test_irc.py
@@ -4,7 +4,7 @@ import atac
 def test_send_email():
     """ """
     #
-    katie = atac.SendIRC(false, "auth.json", None)
+    katie = atac.SendIRC(False, "auth.json", None)
     status = katie.send()
 
     assert (status == 0) is True
