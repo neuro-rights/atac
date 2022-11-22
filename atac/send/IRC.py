5496de5824395b9085fd369197d15eb9d15c5865
x7E3 <x7E3@icloud.com>
2022-11-22 08:56:10 +0100

    irc servers

--- atac/send/IRC.py
+++ atac/send/IRC.py
@@ -725,6 +725,7 @@ class SendIRC(Config):
         factory = IRCFactory(description)
         d = endpoint.connect(factory)
         d.addCallback(lambda protocol: protocol.deferred)
+        
         return d
 
     def send(self, message):
